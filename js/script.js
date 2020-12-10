6d628f2a097ec4c33fa0b5924dcc3701bbc41662
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-12-10 12:36:28 +0100

    header da sistemare

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-    
-  }
+    el: '#app',
+
+    data: {
+        navbarData: [
+            {
+                menuTitle: 'Home',
+                hover: false,
+            },
+            {
+                menuTitle: 'About Me',
+                hover: false,
+            },
+            {
+                menuTitle: 'Testimonials',
+                hover: false,
+            },
+            {
+                menuTitle: 'My Blog',
+                hover: false,
+            },
+            {
+                menuTitle: 'Meetups',
+                hover: false,
+            },
+            {
+                menuTitle: 'Shop',
+                hover: false,
+            },
+            {
+                menuTitle: 'Contact Me',
+                hover: false,
+            },
+
+        ],
+
+        showthis: true,
+    },
+
+    methods: {
+
+
+    }
 })
