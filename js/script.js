470d93910b7cc14fad6fd8489015b8d075f6749f
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-12-11 15:33:01 +0100

    aggiunta parte mancante e sistemato piccole cose

--- js/script.js
+++ js/script.js
@@ -3,54 +3,22 @@ var app = new Vue({
 
     data: {
         navbar: [
-            {
-              menuTitle: 'Home',
-              hover: false,
-            },
-            {
-              menuTitle: 'About Me',
-              hover: false,
-            },
-            {
-              menuTitle: 'Testimonials',
-              hover: false,
-            },
-            {
-              menuTitle: 'My Blog',
-              hover: false,
-            },
-            {
-              menuTitle: 'Meetups',
-              hover: false,
-            },
-            {
-              menuTitle: 'Shop',
-              hover: false,
-            },
-            {
-              menuTitle: 'Contact Me',
-              hover: false,
-            },
-
+          {menuTitle: 'Home', hover: false},
+          {menuTitle: 'About Me', hover: false},
+          {menuTitle: 'Testimonials', hover: false},
+          {menuTitle: 'My Blog', hover: false},
+          {menuTitle: 'Meetups', hover: false},
+          {menuTitle: 'Shop', hover: false},
+          {menuTitle: 'Contact Me', hover: false},
         ],
         footer: [
-            {
-              menuTitle: 'Latest Books',
-            },
-            {
-              menuTitle: 'Upcoming Events',
-            },
-            {
-              menuTitle: 'Recent Articles',
-            },
-            {
-              menuTitle: 'Business Enquiries',
-            },
-            {
-              menuTitle: 'Visit My Foundation',
-            },
+          {menuTitle: 'Latest Books'},
+          {menuTitle: 'Upcoming Events'},
+          {menuTitle: 'Recent Articles'},
+          {menuTitle: 'Business Enquiries'},
+          {menuTitle: 'Visit My Foundation'},
         ],
-        socials:[ 
+        socials:[
           {social:'fab fa-facebook-f', link:'https://it-it.facebook.com/'},
           {social:'fab fa-twitter', link:'https://twitter.com/login?lang=it'},
           {social:'fab fa-instagram', link:'https://www.instagram.com/'},
